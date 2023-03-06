d380e1bbda012e40bc23258027a8e5709527ede6
VINIT K <125847465+Vinit538@users.noreply.github.com>
2023-03-06 21:24:29 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,60 @@
+<head>
+<script>
+function abc()
+{
+var arr=new Array("usepics/ban12.jpg","usepics/ban16.jpg","usepics/ban13.jpg","usepics/ban14.jpg","usepics/ban15.jpg");
+var ind=eval(document.f1.h1.value);
+document.img.src=arr[ind];
+document.f1.h1.value=ind+1;
+if(document.f1.h1.value==5)
+{
+document.f1.h1.value=0;
+}
+}
+setInterval("abc()",3000);
+</script>
+</head>
+<body>
+<div id="RightPart">
+  <div id="Page"><img src="usepics/ban15.jpg" alt="" width="669" height="210" name="img"/>
+  <form name="f1">
+  <input type="hidden" name="h1" value="0" />
+  </form>
+  <div><br/><center><h2><font face="Lucida Handwriting"  color="#00CCFF">Latest Fashion News</font></h2></center></div>
+ <table border="0" cellspacing="20">
+ <tr><td> 
+<img src="usepics/pro1.jpg" width="150" height="180"/>
+</td>
+<td colspan="2">
+<font face="Lucida Handwriting" size="+1" color="#99CC33">
+The 21st century the style trends</font><br>
+<font face="Comic Sans MS"><strong><p>In the 21st century the style trends of the fashion industry dominate the world more than they ever did, and control not only the way people dress but also trends in home ware design, makeup fashion and peoples overall attitudes. In the 60s flower power did not only mean flares and tunics, it summed up the whole attitude of a generation, and this is even more prominent today.
+</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Nowadays, fashion is bold and daring, and this reflects a noughties generation that is not afraid to say what they think, or wear what they want. Fashion is not just a means of clothing your body, it is the essence of your personality and beliefs, and designers are well aware of the power they hold. Designers’ predictions and designs for the coming season are more hotly anticipated than any other revelation in the world.</p></strong></font></td>
+</tr>
+<tr>
+<td>
+<br>
+<img src="usepics/pro11.jpg" width="150" height="180" /><br>
+<font  color="#FF66CC" face="Lucida Handwriting">House Of Sensation</font><br>
+<font face="Comic Sans MS"><strong><br>
+Fashion Men's Latest Coat Pant Designs Casual Business Suit 3 Pieces Set/Men's Suits Blazers Trousers Pants Vest Waistcoat</strong></font></td>
+&nbsp;
+<td>
+<img src="usepics/pro12.jpg" width="150" height="180" /><br>
+<font  color="#FF66CC" face="Lucida Handwriting">Generic Leather Retail Black Color</font><br>
+
+<font face="Comic Sans MS"><strong><br>
+Designer Faux Leather Biker Jacket For Man
+</strong></font></td>
+&nbsp;
+<td>
+<img src="usepics/pro13.jpg" width="150" height="180"/><br>
+<font  color="#FF66C" face="Lucida Handwriting">LookMark Men's Plain </font><br>
+
+<font face="Comic Sans MS"><strong><br>LookMark Men's Plain Regular fit Casual Shirt</strong></font></td>
+</tr>
+</table>
+  </div>
+  </div>
+  </body>
