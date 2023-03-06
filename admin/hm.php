7cbc42a4b970d7225a10be8ac7816cb70aceb582
VINIT K <125847465+Vinit538@users.noreply.github.com>
2023-03-06 20:06:34 +0530

    Add files via upload

---
+++ admin/hm.php
@@ -0,0 +1,8 @@
+<body>
+<br /><br />
+<center><font color="#993399" size="+3" face="Georgia, Times New Roman, Times, serif">Welcome Admin</font><br/><br>
+<img src="usepics/admin login.png" width="300" height="200"/>
+
+</center>
+
+</body>
